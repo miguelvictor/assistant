72c8f662b8240c832a83908ff0c47c5def3eabc9
Miguel Victor <miguel@digitalfans.se>
2025-12-14 00:10:46 +0100

    feat: added shadcn

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,11 +1,9 @@
+import { ComponentExample } from "~/components/component-example"
+
 export function meta() {
   return [{ title: "New React Router App" }, { name: "description", content: "Welcome to React Router!" }]
 }
 
 export default function Home() {
-  return (
-    <main className="p-10">
-      <h1 className="text-4xl font-bold tracking-tight">DF Assistant</h1>
-    </main>
-  )
+  return <ComponentExample />
 }

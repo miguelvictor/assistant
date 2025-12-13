77c00bef0e0ecbdda36eaa77f82b3d562d1a0a3a
Miguel Victor <miguel@digitalfans.se>
2025-12-13 23:34:20 +0100

    Initial commit from create-react-router

---
+++ app/routes/_index.tsx
@@ -0,0 +1,11 @@
+export function meta() {
+  return [{ title: "New React Router App" }, { name: "description", content: "Welcome to React Router!" }]
+}
+
+export default function Home() {
+  return (
+    <main className="p-10">
+      <h1 className="text-4xl font-bold tracking-tight">DF Assistant</h1>
+    </main>
+  )
+}
